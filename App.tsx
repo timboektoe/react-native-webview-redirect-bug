b403d9f8cb29447cfa9de641d9a314019b027d35
Tim van der Heijden <tim.van.der.heijden@securify.nl>
2020-06-22 15:48:53 +0200

    added userAgent prop to make it work with uwv

--- App.tsx
+++ App.tsx
@@ -7,6 +7,9 @@ type AppState = {
     source: WebViewSource;
 }
 
+const IOS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1';
+const ANDROID_USER_AGENT = 'Mozilla/5.0 (Android 4.4; Mobile; rv:46.0) Gecko/46.0 Firefox/46.0';
+
 export default class App extends React.Component<{}, AppState> {
     constructor(props: {}) {
         super(props);
@@ -17,16 +20,27 @@ export default class App extends React.Component<{}, AppState> {
     }
 
     public render(): JSX.Element {
+        const userAgent = Platform.OS === 'ios' ? IOS_USER_AGENT : ANDROID_USER_AGENT;
+
         if (Platform.OS === 'ios') {
             return (
                 <View style={styles.container}>
-                    <WebView style={{ flex: 1 }} source={this.state.source} />
+                    <WebView
+                        userAgent={userAgent}
+                        style={{ flex: 1 }}
+                        source={this.state.source}
+                    />
                 </View>
             );
         } else if (Platform.OS === 'android') {
             return (
                 <View style={styles.container}>
-                    <WebView style={{ flex: 1 }} source={this.state.source} onShouldStartLoadWithRequest={this._handleOnShouldStartLoadWithRequestAndroid}/>
+                    <WebView
+                        userAgent={userAgent}
+                        style={{ flex: 1 }}
+                        source={this.state.source}
+                        onShouldStartLoadWithRequest={this._handleOnShouldStartLoadWithRequestAndroid}
+                    />
                 </View>
             );
         }
@@ -37,11 +51,13 @@ export default class App extends React.Component<{}, AppState> {
 
         if (newURL !== (this.state.source as WebViewSourceUri).uri) {
             console.log(`Set state -  uri: ${newURL}, referer: ${(this.state.source as WebViewSourceUri).uri}`);
+            const refererer = !/login$/gi.test(newURL) ? (this.state.source as WebViewSourceUri).uri : 'https://www.uwv.nl/particulieren/mijnuwv/index.aspx';
+
             this.setState({
                 source: {
                     uri: newURL,
                     headers: {
-                        Referer: !/login$/gi.test(newURL) ? (this.state.source as WebViewSourceUri).uri : 'https://www.uwv.nl/particulieren/mijnuwv/index.aspx'
+                        Referer: refererer
                     }
                 }
             })
