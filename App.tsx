a644671a30d63aa5578f4155415f722705f98dcb
Tim van der Heijden <tim.van.der.heijden@securify.nl>
2021-03-13 16:24:21 +0100

    debugging referer issue

--- App.tsx
+++ App.tsx
@@ -4,19 +4,33 @@ import { WebView } from 'react-native-webview';
 import { WebViewSource, WebViewSourceUri, WebViewNavigation, WebViewNavigationEvent } from 'react-native-webview/lib/WebViewTypes';
 
 type AppState = {
-    source: WebViewSource;
+    uri: string;
+}
+
+let headers = {};
+
+const getHeaders = () => {
+    console.log('I AM CALLED');
+    return headers;
 }
 
 const IOS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1';
-const ANDROID_USER_AGENT = 'Mozilla/5.0 (Android 4.4; Mobile; rv:46.0) Gecko/46.0 Firefox/46.0';
+const ANDROID_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36';
+
+
+//https://www.npmjs.com/package/react-native-referer-webview
+//https://yippeeclip.nl/headers
+//https://github.com/react-native-webview/react-native-webview/issues/460
 
 export default class App extends React.Component<{}, AppState> {
     constructor(props: {}) {
         super(props);
 
         this.state = {
-            source: { uri: 'https://mijn.uwv.nl/' }
+            uri: 'https://www.uwv.nl/particulieren/mijnuwv/index.aspx',
         }
+
+        console.log('************************************************************')
     }
 
     public render(): JSX.Element {
@@ -28,7 +42,10 @@ export default class App extends React.Component<{}, AppState> {
                     <WebView
                         userAgent={userAgent}
                         style={{ flex: 1 }}
-                        source={this.state.source}
+                        source={{
+                            uri: this.state.uri,
+                            headers: getHeaders(),
+                        }}
                     />
                 </View>
             );
@@ -38,7 +55,10 @@ export default class App extends React.Component<{}, AppState> {
                     <WebView
                         userAgent={userAgent}
                         style={{ flex: 1 }}
-                        source={this.state.source}
+                        source={{
+                            uri: this.state.uri,
+                            headers: getHeaders(),
+                        }}
                         onShouldStartLoadWithRequest={this._handleOnShouldStartLoadWithRequestAndroid}
                     />
                 </View>
@@ -49,21 +69,32 @@ export default class App extends React.Component<{}, AppState> {
     private _handleOnShouldStartLoadWithRequestAndroid = (event: WebViewNavigation) => {
         const { url: newURL } = event;
 
-        if (newURL !== (this.state.source as WebViewSourceUri).uri) {
-            console.log(`Set state -  uri: ${newURL}, referer: ${(this.state.source as WebViewSourceUri).uri}`);
-            const refererer = !/login$/gi.test(newURL) ? (this.state.source as WebViewSourceUri).uri : 'https://www.uwv.nl/particulieren/mijnuwv/index.aspx';
+        if (newURL !== this.state.uri) {
+            //let referer = (this.state.source as WebViewSourceUri).uri
 
-            this.setState({
-                source: {
-                    uri: newURL,
-                    headers: {
-                        Referer: refererer
-                    }
-                }
-            })
+            if (
+                // newURL.includes('https://mijn.uwv.nl/iam/digid') ||
+                newURL.startsWith('https://mijn.uwv.nl/iam/login')
+                // newURL.startsWith('https://digid.nl/saml/idp')
+            ) {
+                headers = {
+                    Referer: 'https://www.uwv.nl/particulieren/mijnuwv/index.aspx',
+                    customHeader: 'Fritsie'
+                };
+
+                this.setState({
+                        uri: 'https://yippeeclip.nl/headers',
+                        // uri: 'https://mijn.uwv.nl/iam/login'
+                });
+
+                return false;
+                //referer = 'https://www.uwv.nl/particulieren/mijnuwv/index.aspx';
+            }
+            // const refererer = !/digid$/gi.test(newURL) ? (this.state.source as WebViewSourceUri).uri : 'https://www.uwv.nl/particulieren/mijnuwv/index.aspx';
+            //console.log('0Request URL: ' + newURL.substring(0, 40) + ' with referer: ', referer);
         }
 
-        return false;
+        return true;
     }
 }
 
